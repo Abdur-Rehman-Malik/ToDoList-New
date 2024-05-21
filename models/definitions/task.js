01395a65a18d85c8e406fb63bbad158d9f03deeb
Abdur-Rehman-Malik <abdurrehmanm849@gmail.com>
2024-05-22 01:11:16 +0500

    tasks addded

---
+++ models/definitions/task.js
@@ -0,0 +1,19 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../../bin/dbConnection");
+
+class tasks extends Model {}
+
+tasks.init(
+  {
+    taskId: {
+      primaryKey: true,
+    },
+  },
+  {
+    timestamps: true,
+    paranoid: true,
+    tableName: "tasks",
+    sequelize,
+  }
+);
+module.exports = tasks;
