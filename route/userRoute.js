44c56ad959efee3aa5fac547402a5fc6ece00966
Abdur-Rehman-Malik <abdurrehmanm849@gmail.com>
2024-05-25 13:28:50 +0500

    settting up to do list in main

--- route/userRoute.js
+++
@@ -1,6 +0,0 @@
-const route = require("express").Router();
-const userController = require("../controller/userController");
-
-route.post("/user", userController.createUser);
-
-module.exports = route;
